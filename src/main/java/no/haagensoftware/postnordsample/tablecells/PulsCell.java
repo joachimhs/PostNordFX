5071e624110f09a677eafff27c04d331d7b5fbe1
Joachim Haagen Skeie <joachim@haagen-software.no>
2015-12-17 11:27:08 +0100

    - Legger til text på PulsCell

--- src/main/java/no/haagensoftware/postnordsample/tablecells/PulsCell.java
+++ src/main/java/no/haagensoftware/postnordsample/tablecells/PulsCell.java
@@ -2,10 +2,12 @@ package no.haagensoftware.postnordsample.tablecells;
 
 import javafx.beans.property.StringProperty;
 import javafx.beans.value.ObservableValue;
+import javafx.geometry.VPos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.TableCell;
 import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
 import no.haagensoftware.postnordsample.data.Henteliste;
 import no.haagensoftware.postnordsample.data.PulsItem;
 
@@ -29,21 +31,26 @@ public class PulsCell extends TableCell<Henteliste, String> {
 
         if (!isEmpty() && rowItem != null && rowItem instanceof Henteliste) {
             Henteliste henteliste = (Henteliste)rowItem;
-            generateGraphicsContent(henteliste.getPulsItem());
+            generateGraphicsContent(string, henteliste.getPulsItem());
         }
 
         this.setGraphic(canvas);
     }
 
-    private void generateGraphicsContent(PulsItem pulsItem) {
+    private void generateGraphicsContent(String text, PulsItem pulsItem) {
         double andel = (pulsItem.getGronn() + pulsItem.getGul() + pulsItem.getRod())/100d;
 
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.setFill(Color.GREEN);
-        gc.fillRect(0, 0, pulsItem.getGronn()/andel, 40);
+        gc.fillRect(0, 0, pulsItem.getGronn() / andel, 40);
         gc.setFill(Color.YELLOW);
-        gc.fillRect(pulsItem.getGronn()/andel, 0, pulsItem.getGul()/andel, 40);
+        gc.fillRect(pulsItem.getGronn() / andel, 0, pulsItem.getGul() / andel, 40);
         gc.setFill(Color.RED);
-        gc.fillRect((pulsItem.getGronn() + pulsItem.getGul())/andel, 0, pulsItem.getRod()/andel, 40);
+        gc.fillRect((pulsItem.getGronn() + pulsItem.getGul()) / andel, 0, pulsItem.getRod() / andel, 40);
+
+        gc.setTextAlign(TextAlignment.CENTER);
+        gc.setTextBaseline(VPos.CENTER);
+        gc.setFill(Color.BLACK);
+        gc.fillText(text, 50, 20);
     }
 }
